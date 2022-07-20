4086fb471d3011850b82b6f2da6f5499a861c0db
Enrique <octepc.97@gmail.com>
2022-07-19 19:06:28 -0500

    added some express CRUD

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const router = express.Router();
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hola");
+app.use(router); //de esta manera se añade el router a la aplicación de express
+
+router.get("/message", (req, res) => {
+  res.send("Lista de mensajes");
 });
+
+router.delete("/message", (req, res) => {
+  res.send("Mensaje añadido");
+});
+
+// app.use("/", (req, res) => {
+//   res.send("Hola");
+// });
 
 app.listen(3000);
 console.log("La aplicación está escuchando en http://localhost:3000");
